0d7820824ba73fbc58945e829e268c2b8147328c
Sona Varghese <sonasusan.v@gmail.com>
2020-11-05 03:27:46 +0000

    Commit json_the_cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log(error);
-  } else {
-    if (data.length > 0) {
-      console.log(data[0].description);
+
+const fetchBreedDescription = function(breedName, callback) {
+  request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error || body === '[]') {
+      callback(error, null);
     } else {
-      console.log(`Sorry the breed "${breedName}" does not exist. Try again.`);
+      const data = JSON.parse(body);
+      if (data.length > 0) {
+        callback(null, data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
